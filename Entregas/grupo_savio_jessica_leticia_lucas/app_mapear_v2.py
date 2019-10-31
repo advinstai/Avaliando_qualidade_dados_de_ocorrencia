ea93796d21f466cd68a69620deb003a3f6b7ced8
Havyx <everluca@outlook.com>
2019-10-31 17:13:20 -0300

    Atualizacoes

--- Entregas/grupo_savio_jessica_leticia_lucas/app_mapear_v2.py
+++ Entregas/grupo_savio_jessica_leticia_lucas/app_mapear_v2.py
@@ -200,5 +200,77 @@ class app_grafica(app_hub):
 
 		#Exercicio 04 - Geocode - Verificar se dados batem
 
+		if self.option == app.dicionario[3]:
+			mapa_bio = pd.read_csv(self.path)
+			mapa_bio.rename(columns={'Latitude':'lat','Longitude':'lon'}, inplace=True)
+			row = np.arange(0,len(mapa_bio))
+			loc = []
+			for i in row:
+				loc.append([mapa_bio["lat"][i], mapa_bio["lon"][i]])
+			#locDF = pd.DataFrame(loc,columns=['lat', 'lon'])
+			#st.map(locDF)
+			#st.write(locDF)
+			#
+			# #Reverse geocoder (Precisa terminar)
+			cities =reverse_geocode.search(loc) #Faz processo reverso e através de lat long traz a cidade
+			cities =  pd.DataFrame(cities) #Transforma a lista em dataframe
+			#New dataset com lat long e a respectiva cidade
+			newdf = mapa_bio[['lat','lon','Municipio']]
+			comparecitiesTrue = []
+			comparecitiesFalse = []
+			for j in np.arange(0,len(newdf)):
+				if newdf['Municipio'][j] != cities['city'][j]:
+					comparecitiesFalse.append([mapa_bio['lat'][j],mapa_bio['lon'][j],mapa_bio['Municipio'][j],cities['city'][j]])
+				if newdf['Municipio'][j] == cities['city'][j]:
+					comparecitiesTrue.append([mapa_bio['lat'][j],mapa_bio['lon'][j],mapa_bio['Municipio'][j],cities['city'][j]])
+			comparecitiesFalse = pd.DataFrame(comparecitiesFalse, columns=['lat','lon','Municipio Planilha', 'Reverse Geocode'])
+			comparecitiesTrue = pd.DataFrame(comparecitiesTrue, columns=['lat','lon','Municipio Planilha', 'Reverse Geocode'])
+			st.markdown('### Dados com Localização Correta')
+			#st.map(comparecitiesTrue[['lat','lon']])
+			st.deck_gl_chart(
+    			viewport={
+         		'latitude': -23.37,
+         		'longitude': -51.28,
+         		'zoom': 11,
+         		'pitch': 50,
+     			},
+     			layers=[{
+         		'type': 'HexagonLayer',
+		         'data': comparecitiesTrue[['lat','lon']],
+		         'radius': 200,
+		         'elevationScale': 4,
+		         'elevationRange': [0, 1000],
+		         'pickable': True,
+		         'extruded': True,
+		     	}, {
+		         'type': 'ScatterplotLayer',
+		         'data': comparecitiesTrue[['lat','lon']],
+		     	}])
+			if st.checkbox('Mostrar dados corretos:'):
+				st.write(comparecitiesTrue.loc[:,['Municipio Planilha', 'Reverse Geocode']])
+			st.markdown('### Dados com Localização Incorreta')
+			#st.map(comparecitiesFalse[['lat','lon']])
+			st.deck_gl_chart(
+    			viewport={
+         		'latitude': -23.37,
+         		'longitude': -51.28,
+         		'zoom': 11,
+         		'pitch': 50,
+     			},
+     			layers=[{
+         		'type': 'HexagonLayer',
+		         'data': comparecitiesFalse[['lat','lon']],
+		         'radius': 200,
+		         'elevationScale': 4,
+		         'elevationRange': [0, 1000],
+		         'pickable': True,
+		         'extruded': True,
+		     	}, {
+		         'type': 'ScatterplotLayer',
+		         'data': comparecitiesFalse[['lat','lon']],
+		     	}])
+			if st.checkbox('Mostrar dados incorretos:'):
+				st.write(comparecitiesFalse.loc[:,['Municipio Planilha', 'Reverse Geocode']])
+
 hub_ia = app_grafica()
 hub_ia.inicializar()
